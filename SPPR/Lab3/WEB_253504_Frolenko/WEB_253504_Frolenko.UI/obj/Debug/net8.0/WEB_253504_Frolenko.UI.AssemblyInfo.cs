e8b0b09ac0a09ca74460e8b8545196ccc7f041c5
JankerPlay <kira1950abc@gmail.com>
2024-09-12 18:29:33 +0300

    delete trash

--- SPPR/Lab3/WEB_253504_Frolenko/WEB_253504_Frolenko.UI/obj/Debug/net8.0/WEB_253504_Frolenko.UI.AssemblyInfo.cs
+++ SPPR/Lab3/WEB_253504_Frolenko/WEB_253504_Frolenko.UI/obj/Debug/net8.0/WEB_253504_Frolenko.UI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WEB_253504_Frolenko.UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f292ec8e0cacb4affa55dbeeb9b919928b4fb572")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fac03226c48c40eb826992ae8195f75123984a74")]
 [assembly: System.Reflection.AssemblyProductAttribute("WEB_253504_Frolenko.UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WEB_253504_Frolenko.UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
