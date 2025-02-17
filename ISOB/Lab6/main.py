08f6bc1201d941f39e439bbae28db20687a48d77
Janker <kira1950abc@gmail.com>
2025-02-17 17:09:10 +0300

    add Lab6

---
+++ ISOB/Lab6/main.py
@@ -0,0 +1,43 @@
+import psycopg2
+import sys
+
+def main():
+    enable_protection = True
+    if '--disable-protection' in sys.argv:
+        enable_protection = False
+
+    username = input("Введите имя пользователя: ")
+
+    try:
+        conn = psycopg2.connect(
+            dbname="lab6",
+            user="postgres",
+            password="8025",
+            host="localhost"
+        )
+        cur = conn.cursor()
+
+        if enable_protection:
+            query = "SELECT * FROM users WHERE username = %s"
+            cur.execute(query, (username,))
+        else:
+            query = f"SELECT * FROM users WHERE username = '{username}'"
+            cur.execute(query)
+
+        rows = cur.fetchall()
+        print("Результаты запроса:")
+        for row in rows:
+            print(row)
+
+    except Exception as e:
+        print("Ошибка:", e)
+    finally:
+        if 'cur' in locals():
+            cur.close()
+        if 'conn' in locals():
+            conn.close()
+
+if __name__ == "__main__":
+    main()
+
+# ' OR '1'='1
