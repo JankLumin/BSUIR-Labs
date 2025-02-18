4291c6603472567950152b721f129e4772a54a2c
Janker <kira1950abc@gmail.com>
2025-02-18 15:47:37 +0300

    fix

--- ISOB/Lab4/attack_syn_flood.py
+++ ISOB/Lab4/attack_syn_flood.py
@@ -1,8 +1,9 @@
 import asyncio
 
+
 async def attack_connection(i: int):
     try:
-        reader, writer = await asyncio.open_connection('127.0.0.1', 8888)
+        reader, writer = await asyncio.open_connection("127.0.0.1", 8888)
         challenge = await asyncio.wait_for(reader.readline(), timeout=5)
         print(f"[SYN Flood {i}] Received: {challenge.decode().strip()}")
         await asyncio.sleep(10)
@@ -11,9 +12,11 @@ async def attack_connection(i: int):
     except Exception as e:
         print(f"[SYN Flood {i}] Ошибка: {e}")
 
+
 async def main():
     tasks = [asyncio.create_task(attack_connection(i)) for i in range(20)]
     await asyncio.gather(*tasks)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     asyncio.run(main())
